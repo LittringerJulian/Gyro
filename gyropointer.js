3154a2507c0efa25f15d6be8ac4f3296eff2951a
LittringerJulian <56301487+LittringerJulian@users.noreply.github.com>
2020-11-17 14:19:28 +0100

    gyro in guad

--- gyropointer.js
+++ gyropointer.js
@@ -1,137 +1,72 @@
+var robot = require("robotjs")
+
 module.exports = class Gyropointer {
-    
+
     constructor() {
 
     }
 
-    robot = require("robotjs")
-    
-    start = { alpha: 0, beta: 0 }
-    end = { alpha: 0, beta: 0 }
-    cpoint = { alpha: 0, beta: 0 }
+    screen = { x: robot.getScreenSize().width, y: robot.getScreenSize().height }
 
-    sizeX = 0
-    sizeY = 0
+    mouse = { x: 0, y: 0 }
+    gyroval = { alpha: 0, beta: 0, gamma: 1 }
+    gyroval_old = { alpha: 0, beta: 0, gamma: 1 }
 
 
-    oldPosX = 0
-    oldPosY = 0
+    moveMouse(newvalues) {
+        this.gyroval = newvalues
 
-    multiplierH = 0
-    multiplierV = 0
+        let newx, newy, sens
 
+        if (this.difference(this.gyroval.alpha, this.gyroval_old.alpha) < 180) {
+            // x-
+            if (this.gyroval.alpha > this.gyroval_old.alpha) {
+                newx = -1 * Math.floor(this.distance(this.gyroval.alpha, this.gyroval_old.alpha) * 100) / 100
+            }
+            // x+
+            else {
+                newx = Math.floor(this.distance(this.gyroval.alpha, this.gyroval_old.alpha) * 100) / 100
+            }
 
-// called when pointer.html recalibrates
-// sets the new 'display size'
-// sets start and end points and multiplier for angles
-updateDisplaySize(newstart, newend, newsizeX, newsizeY) {
+        } else {
 
-    this.sizeX = newsizeX
-    this.sizeY = newsizeY
-
-    this.start = newstart
-    this.end = newend
-
-    // multiplier horizontal / vertical
-    this.multiplierH = this.sizeX / this.distance(this.start.alpha, this.end.alpha)
-    this.multiplierV = this.sizeY / this.distance(this.start.beta, this.end.beta)
-}
-
-// called when pointer.html is calibrated
-// and movement is detected
-updatePointer(newcpoint) {
-    this.oldpoint = this.cpoint
-    this.cpoint.alpha = newcpoint.alpha
-    this.cpoint.beta = newcpoint.beta
-    this.updatePointerPos()
-}
-
-// called when pointer position is updated
-updatePointerPos() {
-
-    // variables for new pointer positions on screen
-    var posX
-    var posY
-
-    // calculates new pointer x position
-    //
-    // first case: start angle > end angle
-    if (this.start.alpha >= this.end.alpha) {
-        //  position if the pointer should be on screen
-        if (this.start.alpha >= this.cpoint.alpha && this.end.alpha <= this.cpoint.alpha) {
-            posX = this.distance(this.cpoint.alpha, this.end.alpha) * this.multiplierH
-        }
-        // position if the pointer should be out of screen
-        // aligns pointer to whichever display edge the pointer is closest to
-        else if (this.distance(this.cpoint.alpha, this.start.alpha) < this.distance(this.cpoint.alpha, this.end.alpha)) {
-            posX = this.sizeX
-        }
-        else {
-            posX = 0
-        }
-    }
-    // second case: end angle > start angle
-    else {
-        // position if the pointer should be on screen
-        if ((this.start.alpha >= this.cpoint.alpha && this.end.alpha > this.cpoint.alpha) || (this.start.alpha < this.cpoint.alpha && this.end.alpha <= this.cpoint.alpha)) {
-            posX = this.distance(this.cpoint.alpha, this.end.alpha) * this.multiplierH
-        }
-        // position if the pointer should be out of screen
-        // aligns pointer to whichever display edge the pointer is closest to
-        else if (this.distance(this.cpoint.alpha, this.start.alpha) > this.distance(this.cpoint.alpha, this.end.alpha)) {
-            posX = 0
-        }
-        else {
-            posX = this.sizeX
+            // x+
+            if (this.gyroval.alpha > this.gyroval_old.alpha) {
+                newx = Math.floor(this.distance(this.gyroval.alpha, this.gyroval_old.alpha) * 100) / 100
+            }
+            // x-
+            else {
+                newx = -1 * Math.floor(this.distance(this.gyroval.alpha, this.gyroval_old.alpha) * 100) / 100
+            }
         }
 
+        newy = this.difference(this.gyroval.beta, this.gyroval_old.beta)
+        if (this.gyroval.beta > this.gyroval_old.beta) newy *= -1
+
+        this.gyroval.gamma = Math.round(this.gyroval.gamma * 100) / 100
+        sens = Math.round(((this.gyroval.gamma >= 0) ? ((1 + (Math.abs(this.gyroval.gamma) / 100))) : ((1 - (Math.abs(this.gyroval.gamma) / 100)))) * 30)
+
+        if (newx <= -1 || 1 <= newx) newx += newx * sens
+        if (newy <= -1 || 1 <= newy) newy += newy * sens
+
+        //newx = newx * 20
+        //newy = newy * 20
+
+        robot.moveMouse(Math.round(newx) + robot.getMousePos().x, Math.round(newy) + robot.getMousePos().y)
+
+        this.gyroval_old = this.gyroval
     }
 
-
-    // calculates new pointer x position
-    //
-    // if start > end is only here so the code can be extended more easily
-    // end > start is very unlikely and will never happen unintentionally
-    if (this.start.beta >= this.end.beta) {
-        // position if the pointer should be on screen
-        if (this.start.beta >= this.cpoint.beta && this.end.beta <= this.cpoint.beta) {
-            posY = this.distance(this.cpoint.beta, this.end.beta) * this.multiplierV
-        }
-        // position if the pointer should be out of screen
-        // aligns pointer to whichever display edge the pointer is closest to
-        else if (this.distance(this.cpoint.beta, this.start.beta) < this.distance(this.cpoint.beta, this.end.beta)) {
-            posY = this.sizeY
-        }
-        else {
-            posY = 0
-        }
+    // calculates smallest distance between two angles
+    distance(a, b) {
+        var phi = Math.abs(a - b) % 360
+        var distance = phi > 180 ? 360 - phi : phi
+        return distance
     }
 
-    // prototype for reducing micro movement if the pointer is held still
-    /*if (Math.abs(oldPosX - posX) < 1) {
-        posX = oldPosX
+    // calculates difference between two angles
+    difference(a, b) {
+        return Math.abs(a - b) % 360
     }
-    if (Math.abs(oldPosY - posY) < 1) {
-        posY = oldPosY
-    }
-
-    oldPosX = posX
-    oldPosY = posY
-    */
-
-
-    // returning new pointer position
-    //return (posX, posY)
-
-    //robot.moveMouse(sizeX - posX, sizeY - posY)
-    this.robot.moveMouse(this.sizeX - posX, this.sizeY - posY)
-}
-
-// calculates smallest distance between two angles
-distance(alpha, beta) {
-    var phi = Math.abs(alpha - beta) % 360
-    var distance = phi > 180 ? 360 - phi : phi
-    return distance
-}
 
 }
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io').listen(server)
 const localIpV4Address = require("local-ipv4-address")
-const pass = "19234"
 var localip
 var port = 3000
 
@@ -11,7 +10,7 @@ var robot = require("robotjs")
 
 var gyro = new Gyropointer()
 
-localIpV4Address().then(function (ipAddress) {
+localIpV4Address().then(function(ipAddress) {
     localip = ipAddress
     console.log(localip)
 });
@@ -19,35 +18,18 @@ localIpV4Address().then(function (ipAddress) {
 server.listen(port, () => console.log('app listening on port 3000!'))
 
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', function(socket) {
+    console.log("connected")
 
     //io.emit('getip', "http://" + localip + ":" + port + "/pointer")
 
-
-    socket.on('gyro.recalibrate', function (start, end, password) {
-        if (password == pass) {
-            gyro.updateDisplaySize(start, end, robot.getScreenSize().width, robot.getScreenSize().height)
-        }
+    socket.on('gyro.moveMouse', function(gyroval) {
+        gyro.moveMouse(gyroval)
     })
+});
 
-    socket.on('gyro.movepointer', function (cpoint, password) {
-        if (password == pass) {
-            gyro.updatePointer(cpoint)
-        }
-    })
-
-    socket.on('gyro.mousebuttontoggle', function (mousestatus, password) {
-        if (password == pass) {
-            if (!mousestatus) {
-                robot.mouseToggle("down")
-            }
-            else {
-                robot.mouseToggle("up")
-            }
-        }
-    })
-
-    console.log("connected")
+app.get('/gyroold', (req, res) => {
+    res.sendFile(__dirname + '/gyroold.html')
 });
 
 app.get('/gyro', (req, res) => {
